63cdf9cab521e00cdf1419ce60604fc9580edd5b
Hysis01 <raynorj68@gmail.com>
2023-04-13 16:15:50 +0300

    тест таймдельта match case (без else)

--- test.py
+++ test.py
@@ -19,19 +19,18 @@ try:
         hours = delta.seconds // 3600
         minutes = (delta.seconds - hours * 3600) // 60
         days = delta.days
-        match (True):
-            case True if days == 0 and hours == 0:
-                print('До часа "Икс" ' + choose_plural(minutes, ["минута", "минуты", "минут"]))
-            case True if days == 0 and hours != 0:
-                print('До часа "Икс" ' + choose_plural(hours,["час", "часа", "часов"]) + " и " +
-                      choose_plural(minutes, ["минута", "минуты", "минут"]))
-            case True if days != 0 and minutes == 0:
-                print('До часа "Икс" ' + choose_plural(days, ["день", "дня", "дней"]) + " и " +
-                      choose_plural(hours, ["час", "часа", "часов"]))
-            case _:
-                print('До часа "Икс" ' + choose_plural(days, ["день", "дня", "дней"]) + " " +
-                      choose_plural(hours, ["час", "часа", "часов"]) + " " +
-                      choose_plural(minutes, ["минута", "минуты", "минут"]))
+        if days == 0 and hours == 0:
+            print('До часа "Икс" ' + choose_plural(minutes, ["минута", "минуты", "минут"]))
+        if days == 0 and hours != 0:
+            print('До часа "Икс" ' + choose_plural(hours,["час", "часа", "часов"]) + " и " +
+                  choose_plural(minutes, ["минута", "минуты", "минут"]))
+        if days != 0 and minutes == 0:
+            print('До часа "Икс" ' + choose_plural(days, ["день", "дня", "дней"]) + " и " +
+                choose_plural(hours, ["час", "часа", "часов"]))
+        if days != 0 and hours != 0 and minutes != 0:
+            print('До часа "Икс" ' + choose_plural(days, ["день", "дня", "дней"]) + " " +
+                choose_plural(hours, ["час", "часа", "часов"]) + " " +
+                choose_plural(minutes, ["минута", "минуты", "минут"]))
     else:
         print('Ошибка')
 except:
